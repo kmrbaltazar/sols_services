c8fb094327c09c53ce793260edf361dd2f441f12
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-06-05 20:38:55 +1200

    Made responsive footer

--- footer.php
+++ footer.php
@@ -1,13 +1,29 @@
 </div> 
         <!-- this is where page-content div closes -->
         <footer>
-            <h1 class='footerTest'>FOOTER CONTENT</h1>
-                <nav>
-                    <?php
-                        $argument = array( 'theme_location' => 'footer' );
-                        wp_nav_menu( $argument );
-                    ?>
-                </nav>
+             <a href="<?php echo home_url(); ?>" class="logo"> 
+                <div class="logo-container">
+                    <img src="<?php bloginfo('template_url'); ?>/Assets/sols-services-logo-v2.png" alt="Sol's Services Logo">
+                </div>
+            </a>
+            <div class="footer-text-container">
+                <div>
+                    <div class="contact-details-div">
+                        <p class="paragraph2">158 Leaver Terrace, North New Brighton
+    Christchurch, New Zealand</p>
+                        <p class="paragraph2">020 414 94452</p>
+                        <p class="paragraph2">arrowfieldsol@gmail.com</p>
+                    </div>
+                    <div class="social-icons-div">
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+                <div class="copyright-div">
+                    <p class="dev-details">Web design and development by <a href="#" class="tin-logo-container"><img src="<?php bloginfo('template_url'); ?>/Assets/tin-logo-white.png" alt="Tin Baltazar logo" class="tin-logo"></a></p>
+                    <p>Copyright © 2023 All Rights Reserved </p>
+                </div>
+            </div>
         </footer>
         
         
