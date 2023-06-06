3888ac2ed01a2e1f39273b85edd7de7db6f10cf1
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-06-07 11:29:52 +1200

    Added Featured services section on landing (desktop only)

--- index.php
+++ index.php
@@ -8,16 +8,42 @@
         <div class="home-landing-content">
             <div>
                 <h4 class="h4 home-landing-text">Hi, my name is Sol, my team and I offer</h4>
+                <br>
                 <h1 class="headline home-landing-text">High Quality Gardening and Landscaping Services</h1>
             </div>
             <div class="button-style support-btn">
-                <span><a href="<?php echo home_url(); ?>/contact/" class="button">Call now 02041494452</a></span>
+                <span><a href="tel:+642041494452" class="button">Call now 02041494452</a></span>
             </div>  
 
         </div>
     </section>
+
+    <section class="landing-services">
+        <h2 class="h2">WHAT WE DO</h2>
+        <div class="featured-services-cards">
+            <div class="featured-card">
+                <a href="<?php echo home_url(); ?>/gardening/">
+                    <img src="<?php bloginfo('template_url'); ?>/Assets/landing-gardening.jpg" alt="Gardening photo">
+                    <div class="featured-title">
+                        <h3 class="h3">Gardening</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="featured-card">
+                <a href="<?php echo home_url(); ?>/landscaping/">
+                    <img src="<?php bloginfo('template_url'); ?>/Assets/landing-landscaping.jpg" alt="Landscaping photo">
+                    <div class="featured-title">
+                        <h3 class="h3">Landscaping</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="button-style featured-btn">
+            <span><a href="<?php echo home_url(); ?>/services/" class="button">View Services</a></span>
+        </div>
+    </section>
     
-    <hr>
+   
 <!-- below this comment it will be footer.php -->
 <?php
     get_footer();
