c034698ec63336e5c011da4ce6f6baf6532dc7d2
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-06-08 11:38:03 +1200

    Removed People custom post

--- functions.php
+++ functions.php
@@ -10,20 +10,3 @@ register_nav_menus(array(
     'footer' => __( 'Footer Menu'),
     'dropdown' => __( 'Dropdown Menu')  
 ));
-// Custom Post Type Creation
-function create_posttype() {
-    register_post_type( 'wp_person',
-      array(
-        'labels' => array(
-          'name' => __( 'People' ),
-          'singular_name' => __( 'Person' )
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array('slug' => 'people'),
-      )
-    );
-  }
-  add_action( 'init', 'create_posttype' );
-// below line enables 'Featured Images' tab to emerge on dashboard for posts/pages/customposts
-add_theme_support( 'post-thumbnails' );
