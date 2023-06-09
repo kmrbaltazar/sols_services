03c88eb8b316d53a782b0385b9e39e617fe6b358
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-06-09 12:10:02 +1200

    Fixed gardening and landscaping responsive pages

--- page-landscaping.php
+++ page-landscaping.php
@@ -9,11 +9,10 @@
    <section class="main-service-intro">
         <div class="main-service-image-div">
             <div class="main-service-image-wrapper">
-                <img src="<?php bloginfo('template_url'); ?>/Assets/services-carousel6.png" alt="Gardening project">
+                <img src="<?php bloginfo('template_url'); ?>/Assets/landscaping-main.jpg" alt="Landscaping project">
             </div>
             <div class="main-service-price-div">
-                <h3 class="h3">From $60 per hour + GST</h3>
-                <h3 class="h3">Minimum charge: $138</h3>
+                <h3 class="h3">Price by estimate</h3>
             </div>
         </div>
         <div class="main-service-blurb">
@@ -25,18 +24,18 @@
 
    <section class="offerings-section">
         <?php
-            $gardening_services = pods('gardening_job');
+            $landscaping_services = pods('landscaping_job');
 
             $params = array(
                 'orderby' => 'date',    // Sort by date
                 'order' => 'ASC'        // Display in ascending order (oldest first)
             );
 
-            $gardening_services->find($params);
+            $landscaping_services->find($params);
 
-            while ($gardening_services->fetch()) {
-                $icon_class = $gardening_services->display('icon_name');
-                $name = $gardening_services->display('service_name');
+            while ($landscaping_services->fetch()) {
+                $icon_class = $landscaping_services->display('icon_name');
+                $name = $landscaping_services->display('service_name');
                 ?>
                 <div class="gardening-service">
                     <i class="fa-solid fa-<?php echo $icon_class; ?>"></i>
@@ -49,26 +48,26 @@
 
    <hr>
 
-   <h2 class="h2 sub-title">OUR GARDENING PROJECTS</h2>
+   <h2 class="h2 sub-title">OUR LANDSCAPING PROJECTS</h2>
 
    <section class="service-project-section">
         <?php 
         // you can change variable name to anything you want, just make sure to change it for all of them
-        $gardening_featured_product = pods( 'gardening_featured_p' );  // change 'pod-name' to the Pods custom post name
-        $gardening_featured_product->find();
-        while ( $gardening_featured_product->fetch() ) {
+        $landscaping_featured_product = pods( 'landscaping_featured' );  // change 'pod-name' to the Pods custom post name
+        $landscaping_featured_product->find();
+        while ( $landscaping_featured_product->fetch() ) {
             ?>
             <div class='service-before-after-gallery'>
                 <div class="service-before">
-                    <h4 class="h4 name-before"> <?php echo $gardening_featured_product->display( 'project_name' ); ?> </h4> 
+                    <h4 class="h4 name-before"> <?php echo $landscaping_featured_product->display( 'project_name' ); ?> </h4> 
                     <div class="service-image-wrapper">
-                        <img src="<?php echo $gardening_featured_product->display( 'before_image' ); ?>" alt="Project Before Image">
+                        <img src="<?php echo $landscaping_featured_product->display( 'before_image' ); ?>" alt="Project Before Image">
                     </div>
                 </div>
                 <div class="service-after">
-                    <h4 class="h4 name-after"> <?php echo $gardening_featured_product->display( 'project_name' ); ?> </h4> 
+                    <h4 class="h4 name-after"> <?php echo $landscaping_featured_product->display( 'project_name' ); ?> </h4> 
                     <div class="service-image-wrapper">
-                        <img src="<?php echo $gardening_featured_product->display( 'after_image' ); ?>" alt="Project After Image">
+                        <img src="<?php echo $landscaping_featured_product->display( 'after_image' ); ?>" alt="Project After Image">
                     </div>
                 </div>
             </div> 
